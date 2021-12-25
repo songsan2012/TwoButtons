4388c5ca5fa1cc2372ae87c12e7f7143addeeb0d
Song San <songsan2012@gmail.com>
2021-12-25 09:45:37 -0800

    Initial Commit

---
+++ TwoButtons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwoButtons
+//
+//  Created by song on 12/25/21.
+//  Copyright © 2021 song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
