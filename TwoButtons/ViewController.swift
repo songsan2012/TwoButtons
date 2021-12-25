b90141b4e536889e86a79749a8814443dd1d59d7
Song San <songsan2012@gmail.com>
2021-12-25 10:01:13 -0800

    2 buttons plus complete

--- TwoButtons/ViewController.swift
+++ TwoButtons/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // On load set to 0
+        messageLabel.text = String(counter)
     }
 
-
+    @IBAction func plus1ButtonPressed(_ sender: Any) {
+        
+        counter = counter + 1
+        print("Plus 1")
+        
+        messageLabel.text = String(counter)
+        
+    }
+    
+    @IBAction func plus2ButtonPressed(_ sender: Any) {
+        
+        counter = counter + 2
+        print("Plus 2")
+        messageLabel.text = String(counter)
+    }
+    
+    
 }
 
